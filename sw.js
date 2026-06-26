7381f8628967836845a7378dce6c5813877e446b
1195689456houjunchen <1195689456houjunchen@gmail.com>
2026-06-26 11:42:01 +0800

    fix: 更新SW缓存版本bazi-v2，强制清除旧缓存

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bazi-v1';
+const CACHE = 'bazi-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
