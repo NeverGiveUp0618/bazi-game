78a5ba4b06f6565d253dbdbe5bc709008fc5dbc8
1195689456houjunchen <1195689456houjunchen@gmail.com>
2026-07-19 18:02:34 +0800

    增强页面返回滚动位置恢复

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bazi-v5';
+const CACHE = 'bazi-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
